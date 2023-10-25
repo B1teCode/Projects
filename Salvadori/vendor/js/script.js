b5232d51246aa78269e9152c29b4e91df93fab36
B1teCode <ardasher.gaynazarov@mail.ru>
2023-10-26 00:15:57 +0300

    completed view of the menu part

--- Salvadori/vendor/js/script.js
+++ Salvadori/vendor/js/script.js
@@ -1,10 +1,2 @@
-$(function() {
-    $(".navigation-tab-item").click(function() {
-      $(".navigation-tab-item").removeClass("active");
-      $(this).addClass("active");
-      $(".navigation-tab-overlay").css({
-        left: $(this).prevAll().length * 130 + "px"
-      });
-    });
-  });
+
   
